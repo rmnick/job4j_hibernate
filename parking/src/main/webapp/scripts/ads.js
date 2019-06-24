7372580a1388f839c06acb1689957b3f68ed26b3
rmnick <r.m.nick@yandex.ru>
2019-06-25 02:42:21 +0500

    so slow

--- parking/src/main/webapp/scripts/ads.js
+++ parking/src/main/webapp/scripts/ads.js
@@ -1,1 +1,6 @@
-console.log("from ads");
+// Add the name of the file appear on select
+$(".custom-file-input").on("change", function() {
+    console.log("in script");
+    var fileName = $(this).val().split("\\").pop();
+    $(this).siblings(".custom-file-label").addClass("selected").html(fileName);
+});
