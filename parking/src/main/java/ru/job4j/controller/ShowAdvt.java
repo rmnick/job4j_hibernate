4a98987592d0835917f98a269559ed9d6a967895
rmnick <r.m.nick@yandex.ru>
2019-07-01 15:59:11 +0500

    ???

--- parking/src/main/java/ru/job4j/controller/ShowAdvt.java
+++ parking/src/main/java/ru/job4j/controller/ShowAdvt.java
@@ -1,6 +1,7 @@
 package ru.job4j.controller;
 
 import org.apache.log4j.Logger;
+import org.codehaus.jackson.map.ObjectMapper;
 import ru.job4j.service.Service;
 import ru.job4j.service.entities.Advertisement;
 
@@ -10,6 +11,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.*;
+import java.util.Base64;
 import java.util.List;
 
 public class ShowAdvt extends HttpServlet {
@@ -18,38 +20,59 @@ public class ShowAdvt extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        try {
-            List<Advertisement> ads = service.getAllAds();
-            String path = ads.get(1).getPicturePath();
+        ObjectMapper mapper = new ObjectMapper();
+        List<Advertisement> ads = service.getAllAds();
+        Advertisement ad = ads.get(0);
+//        for (Advertisement ad : ads) {
+            String base64Image = "";
+            File file = new File(ad.getPicturePath());
+            try (FileInputStream imageInFile = new FileInputStream(file)) {
+                // Reading a Image file from file system
+                byte imageData[] = new byte[(int) file.length()];
+                imageInFile.read(imageData);
+                base64Image = Base64.getEncoder().encodeToString(imageData);
+                System.out.println(base64Image);
+                PrintWriter writer = resp.getWriter();
+                writer.println(base64Image);
+                writer.flush();
 
-            ServletContext cntx = req.getServletContext();
-            // Get the absolute path of the image
-            String filename = path;
-            // retrieve mimeType dynamically
-            String mime = cntx.getMimeType(filename);
-            if (mime == null) {
-                resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-                return;
+            } catch (FileNotFoundException e) {
+                System.out.println("Image not found" + e);
+            } catch (IOException ioe) {
+                System.out.println("Exception while reading the Image " + ioe);
             }
-            resp.setContentType(mime);
-            File file = new File(filename);
-            resp.setContentLength((int) file.length());
 
-            FileInputStream in = new FileInputStream(file);
-            OutputStream out = resp.getOutputStream();
+//            for (Advertisement ad : ads) {
+//                String path = ad.getPicturePath();
+//
+//                ServletContext cntx = req.getServletContext();
+//                // Get the absolute path of the image
+//                String filename = path;
+//                // retrieve mimeType dynamically
+//                String mime = cntx.getMimeType(filename);
+//                if (mime == null) {
+//                    resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+//                    return;
+//                }
+//                resp.setContentType(mime);
+//                File file = new File(filename);
+//                resp.setContentLength((int) file.length());
+//
+//                FileInputStream in = new FileInputStream(file);
+//
+//                // Copy the contents of the file to the output stream
+//                byte[] buf = new byte[1024];
+//                int count = 0;
+//                while ((count = in.read(buf)) >= 0) {
+//                    out.write(buf, 0, count);
+//                }
+//                in.close();
+//            }
+//
+////            out.write(String.format("{car:%s}", "Kia").getBytes());
+//            out.flush();
+//            out.close();
 
-            // Copy the contents of the file to the output stream
-            byte[] buf = new byte[1024];
-            int count = 0;
-            while ((count = in.read(buf)) >= 0) {
-                out.write(buf, 0, count);
-            }
-            out.write(String.format("{car:%s}", "Kia").getBytes());
-            out.flush();
-            out.close();
-            in.close();
-        } catch (IOException e) {
-            LOG.error(e.getMessage(), e);
-        }
+//        }
     }
 }
--- parking/src/main/webapp/scripts/showAdvt.js
+++ parking/src/main/webapp/scripts/showAdvt.js
@@ -1,11 +1,29 @@
-$(document).ready(function () {
+// $(document).ready(function () {
+//     $.ajax({
+//         url: "./showAds",
+//         method: "get",
+//         success: function (data) {
+//         }
+//     });
+// });
+$(function () {
+    getImg(function (data) {
+        var img = window.atob(data);
+        var table = $("table tbody");
+        table.empty();
+        var row = "<tr></tr><td><img src=data:image/jpeg;base64, " + data + "/></td><td>desc</td><td>false</td></tr>";
+        table.append(row);
+        console.log(data);
+    })
+})
+
+
+function getImg(convert) {
     $.ajax({
         url: "./showAds",
         method: "get",
         success: function (data) {
-            var table = $("table tbody");
-            var row = "<tr></tr><td><img src='./showAds' class='img-fluid' alt='Responsive image'></td><td>desc</td><td>false</td></tr>";
-            table.append(row);
+            convert(data);
         }
     });
-});
+}
